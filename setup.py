aeaf19e2a555efed7bdd1d5a2d71aaa801b530c8
Simon Sanchez V <simonsanvil2012@gmai.com>
2021-09-20 18:25:48 +0200

    Build wheels for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A collection of Python tools for programming and data science to make my life easier',
+    author='Simon Sanchez Viloria',
+    license='MIT',
+)
