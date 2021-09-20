57bbc9a1f9c02f3fa731e957a524c5e6cab0ca2c
Simon Sanchez V <simonsanvil2012@gmai.com>
2021-09-20 18:27:49 +0200

    changed name in setup.py and modified README

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ pyhandy
 ==============================
 
 A collection of Python tools for programming and data science to make my life easier
+
+Installation
+------------
+```
+pip install git+git://github.com/simonsanvil/pyhandy.git
+```
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='pyhandy',
     packages=find_packages(),
     version='0.1.0',
     description='A collection of Python tools for programming and data science to make my life easier',
